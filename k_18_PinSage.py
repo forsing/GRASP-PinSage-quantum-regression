06eba28acf0a7b26002aee638d5eeede55c1dbaa
ForsIng <forsing@gmail.com>
2026-03-17 20:54:32 +0100

    Update CSV path and modify prediction output

--- k_18_PinSage.py
+++ k_18_PinSage.py
@@ -23,7 +23,7 @@ from qiskit_machine_learning.utils import algorithm_globals
 from qiskit.circuit.library import ZZFeatureMap, TwoLocal
 from qiskit.quantum_info import Statevector, Pauli
 
-CSV_PATH = "/Users/4c/Desktop/GHQ/data/loto7hh_4580_k21.csv"
+CSV_PATH = "/data/loto7hh_4580_k21.csv"
 
 df = pd.read_csv(CSV_PATH)
 print()
@@ -205,7 +205,7 @@ def main():
     print()
     """
     Predikcija (PinSage + kvantna regresija, deterministički, strukturno):
-    (8, 11, 22, 23, 26, 33, 34)
+    (8, 11, x, y, z, 33, 34)
 
     Score: 0.5001153083135611
     """
